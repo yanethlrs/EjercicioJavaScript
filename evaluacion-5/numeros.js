ed8d72b22d99609b99c7488937c4ff89057f582e
Yaneth Rodríguez <108439137+yanethlrs@users.noreply.github.com>
2023-06-21 19:52:06 -0500

    evaluacion numeros

---
+++ evaluacion-5/numeros.js
@@ -0,0 +1,11 @@
+let altura_cm = 160;
+let altura_m = 1.60;
+let peso_k = 58.5;
+let altura_arriba = altura_m.toFixed(0);
+let altura_abajo = parseInt(peso_k);
+let maximo = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+
+console.log(altura_arriba);
+console.log(altura_abajo);
+console.log(maximo);
