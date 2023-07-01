018b1601aad4d0131251389fa6898bbd973af06e
Yaneth Rodríguez <108439137+yanethlrs@users.noreply.github.com>
2023-06-30 20:14:01 -0500

    ejercicio3

---
+++ evaluacion-7/ejercicio-3.js
@@ -0,0 +1,15 @@
+const fechahoy = new Date();
+const fechanac = new Date(1979, 10, 7);
+const varfecha = fechahoy > fechanac;
+const dianac = fechanac.getDate();
+const mesnac = fechanac.getMonth();
+const annonac = fechanac.getFullYear();
+
+
+console.log(fechahoy);
+console.log(fechanac);
+console.log(varfecha);
+console.log(dianac);
+console.log(mesnac);
+console.log(annonac);
+
