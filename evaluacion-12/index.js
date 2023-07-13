08b7bd27ee34d726df69f324524f915e929863bb
Yaneth Rodríguez <108439137+yanethlrs@users.noreply.github.com>
2023-07-12 20:47:18 -0500

    Update index.js

--- evaluacion-12/index.js
+++ evaluacion-12/index.js
@@ -1,13 +1,14 @@
 function fib(num) {
     let a = 1;
     let b = 1;
+    let lista = [1, 1];
     for (let i = 3; i <= num; i++) {
       let c = a + b;
       a = b;
       b = c;
+      lista.push(c);
     }
-    return b;
+    return lista;
+    console.log(lista)
   }
-
-  console.log(fib(3));
-  console.log(fib(7));
+  console.log(fib(6));
