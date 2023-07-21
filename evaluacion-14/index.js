aac85cdc8d18936390d834fbb6f8f1abf4c79151
Yaneth Rodríguez <108439137+yanethlrs@users.noreply.github.com>
2023-07-20 19:59:04 -0500

    evaluacion-14

---
+++ evaluacion-14/index.js
@@ -0,0 +1,8 @@
+const boton = document.getElementById('btn');
+boton.addEventListener('click', evento => {
+    alert("Click en el botón")
+});
+
+$('#btn').click (function() {
+   console.log("Hola, estoy utilizando jQuery")
+})
