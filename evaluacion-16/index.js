da58c3abcc68867788d33c9577c5fc27f2233655
Yaneth Rodríguez <108439137+yanethlrs@users.noreply.github.com>
2023-07-23 19:04:27 -0500

    evaluacion-16

---
+++ evaluacion-16/index.js
@@ -0,0 +1,48 @@
+const parrafos = document.querySelectorAll(".parrafo")
+const secciones = document.querySelectorAll(".seccion")
+const papeleras = document.querySelectorAll(".papelera")
+
+parrafos.forEach(parrafo => {
+    parrafo.addEventListener("dragstart", event => {
+        console.log("Estoy arrastrando el párrafo: " + parrafo.innerText)
+        parrafo.classList.add("dragging")
+        event.dataTransfer.setData("id", parrafo.id)
+        const elemento_fantasma = document.querySelector(".imagen-fantasma")
+        event.dataTransfer.setDragImage(elemento_fantasma, 0, 0)
+    })
+
+    parrafo.addEventListener("dragend", () => {
+        // console.log("He terminado de arrastrar")
+        parrafo.classList.remove("dragging")
+    })
+})
+
+secciones.forEach(seccion => {
+    seccion.addEventListener("dragover", event => {
+        event.preventDefault()
+        event.dataTransfer.dropEffect = "copy" // copy por defecto
+        // console.log("Drag Over")
+        //
+    })
+
+    seccion.addEventListener("drop", event => {
+        console.log("Drop")
+        const id_parrafo = event.dataTransfer.getData("id")
+        // console.log("Párrafo id: ", id_parrafo)
+        const parrafo = document.getElementById(id_parrafo)
+        seccion.appendChild(parrafo)
+    })
+})
+
+papeleras.forEach(papelera => {
+    papelera.addEventListener("dragover", event => {
+        event.preventDefault()
+        event.dataTransfer.dropEffect = "copy"
+    })
+
+    papelera.addEventListener("drop", event => {
+        const id_parrafo = event.dataTransfer.getData("id")
+        const parrafo = document.getElementById(id_parrafo)
+        document.getElementById("id_parrafo").remove()
+    })
+})
